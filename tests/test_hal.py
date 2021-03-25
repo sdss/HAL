17d26fd17d171ebe47dbc568f47b4362d51b04bb
José Sánchez-Gallego <gallegoj@uw.edu>
2021-03-24 17:17:44 -0700

    Add placeholder test

---
+++ tests/test_hal.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# @Author: José Sánchez-Gallego (gallegoj@uw.edu)
+# @Date: 2021-03-24
+# @Filename: test_hal.py
+# @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
+
+
+def test_placeholder():
+
+    assert True
