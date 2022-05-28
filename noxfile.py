dd0809c7c5b8aacb43bdb1fa20eaa07de54d532b
José Sánchez-Gallego <gallegoj@uw.edu>
2022-05-28 11:50:39 -0700

    Watch hal directory with sphinx-autobuild

--- noxfile.py
+++ noxfile.py
@@ -42,6 +42,7 @@ def docs_live(session):
                 # for sphinx
                 "-b=dirhtml",
                 "-a",
+                "--watch=../../src/hal",
                 docs_dir,
                 destination,
                 external=True,
